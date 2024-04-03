de5d0527c2e20a01fff2d54c72df5b8cf56225ca
unicodeee <unicodeee2000@gmail.com>
2024-04-02 17:43:12 -0700

    added stars

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import './style.css'
 // import { setupCounter } from './counter.ts'
 
 import * as THREE from 'three';
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {s} from "vite/dist/node/types.d-aGj9QkWt";
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -34,6 +35,16 @@ const ambientLight =  new THREE.AmbientLight(0xffffff);
 
 const lightHelper = new THREE.PointLightHelper(pointLight);
 
+
+const controls = new OrbitControls( camera, renderer.domElement );
+
+
+const size = 10;
+const divisions = 10;
+
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
 scene.add(lightHelper);
 
 
@@ -42,8 +53,26 @@ scene.add(pointLight);
 scene.add(ambientLight);
 scene.add(torus);
 
+
+function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+
+    const [x, y, z] = Array(3)
+        .fill()
+        .map(() => THREE.MathUtils.randFloatSpread(100));
+
+    star.position.set(x, y, z);
+    scene.add(star);
+
+}
+
+Array(100).fill().forEach(addStar);
+
 function animate() {
     requestAnimationFrame( animate );
+    controls.update();
     torus.rotation.x += 0.001;
     torus.rotation.y += 0.005;
     renderer.render( scene, camera );
